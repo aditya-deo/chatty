2fb9212aebb058a8afdb1df84dda1a9239598892
TECOA136_Aditya_ Deo <ahdeo2015@gmail.com>
2021-08-31 16:55:03 +0530

    add functionality

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const io = require("socket.io")(8000);
-
+const io = require("socket.io")(8000, {cors: {origin: '*',}});
 const users = {};
 
 io.on("connection", function(socket){
-    socket.on('new-user-joined', function(name){
+    socket.on('new-user-joined', name=>{
+        console.log(name + " joined the chat.");
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
 
 
-    socket.on('send', function(message){
+    socket.on('send', message=>{
         socket.broadcast.emit('receive', {message: message, name: users[socket.id]});
     });
 })
